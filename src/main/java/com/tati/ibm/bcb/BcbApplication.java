efb15b6e2febde880052147a7bcd1b3d67d09960
Tati Ramos <tatiramos@ibm.com>
2023-03-14 19:31:20 -0300

    Project Created

---
+++ src/main/java/com/tati/ibm/bcb/BcbApplication.java
@@ -0,0 +1,13 @@
+package com.tati.ibm.bcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tati/ibm/bcb/BcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tati.ibm.bcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
