39328d7b8d49502d15b65e0890b5dc7751a21e93
Tati Ramos <tatiramos@ibm.com>
2023-03-16 14:20:18 -0300

    ✨PUT and POST methods working. Response and Request implementation successfully”

--- src/main/java/com/tati/ibm/bcb/request/MonthlyInterestRateRequest.java
+++ src/main/java/com/tati/ibm/bcb/request/MonthlyInterestRateRequest.java
@@ -1,11 +1,17 @@
 package com.tati.ibm.bcb.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonthlyInterestRateRequest {
 
     @JsonProperty("id")
--- src/main/java/com/tati/ibm/bcb/response/MonthlyInterestRateResponse.java
+++ src/main/java/com/tati/ibm/bcb/response/MonthlyInterestRateResponse.java
@@ -18,35 +18,26 @@ public class MonthlyInterestRateResponse {
     private Long id;
 
     @JsonProperty("Mes")
-    @NotBlank(message = "O campo 'mes' não pode ser vazio")
     private String mes;
 
     @JsonProperty("Modalidade")
-    @NotBlank(message = "O campo 'modalidade' não pode ser vazio")
     private String modalidade;
 
     @JsonProperty("Posicao")
-    @NotNull(message = "O campo 'posicao' não pode ser nulo")
     private Integer posicao;
 
     @JsonProperty("InstituicaoFinanceira")
-    @NotBlank(message = "O campo 'instituicaoFinanceira' não pode ser vazio")
     private String instituicaoFinanceira;
 
     @JsonProperty("TaxaJurosAoMes")
-    @NotNull(message = "O campo 'taxaJurosAoMes' não pode ser nulo")
     private Double taxaJurosAoMes;
 
     @JsonProperty("TaxaJurosAoAno")
-    @NotNull(message = "O campo 'taxaJurosAoAno' não pode ser vazio")
     private Double taxaJurosAoAno;
 
     @JsonProperty("cnpj8")
-    @NotBlank(message = "O campo 'cnpj8' não pode ser vazio")
-    @Length(min = 1, max = 8, message = "O campo 'cnpj8' deve ter entre 1 e 8 caracteres.")
     private String cnpj8;
 
     @JsonProperty("anoMes")
-    @NotBlank(message = "O campo 'anoMes' não pode ser vazio")
     private String anoMes;
 }
